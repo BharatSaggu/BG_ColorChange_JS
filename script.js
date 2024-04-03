1e720712ab779c0fc7f888ddc44a5dc376f98d47
Bharat Bhushan <136876450+BharatSaggu@users.noreply.github.com>
2024-04-03 13:07:03 +0530

    initiat

---
+++ script.js
@@ -0,0 +1,72 @@
+console.log("Jai Shree Ram");
+
+
+// Array.from(button).forEach(()=>{});
+
+let red =()=>{
+    
+     let lal = "#f90505";
+     
+     document.body.style.backgroundColor=lal;
+     
+
+};
+document.getElementById('red').addEventListener('click',red);
+ 
+
+
+let orange =()=>{
+    
+     let lal = "rgb(245, 131, 9)";
+     document.body.style.backgroundColor=lal;
+     
+
+};
+ 
+document.getElementById('orange').addEventListener('click',orange);
+
+
+let yellow =()=>{
+    
+     let lal = "rgb(127, 127, 2)";
+     document.body.style.backgroundColor=lal;
+     
+
+};
+ 
+document.getElementById('yellow').addEventListener('click',yellow);
+
+
+let skyblue =()=>{
+    
+     let lal = "rgb(99, 207, 249)";
+     document.body.style.backgroundColor=lal;
+     
+
+};
+ 
+document.getElementById('skyblue').addEventListener('click',skyblue);
+
+
+let greenyellow =()=>{
+    
+     let lal = "rgb(95, 154, 6)";
+     document.body.style.backgroundColor=lal;
+     
+
+};
+ 
+document.getElementById('greenyellow').addEventListener('click',greenyellow);
+
+
+let pink =()=>{
+    
+     let lal = "rgb(246, 139, 157)";
+     document.body.style.backgroundColor=lal;
+     
+
+};
+ 
+document.getElementById('pink').addEventListener('click',pink);
+
+
